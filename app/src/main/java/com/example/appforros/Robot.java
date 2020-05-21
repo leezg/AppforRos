01da96b59c1c16ca1d4415f1e3dbc9a144199808
leezg <2655421818@qq.com>
2020-05-21 19:57:49 +0800

    实现手机到电脑的通信

--- app/src/main/java/com/example/appforros/Robot.java
+++ app/src/main/java/com/example/appforros/Robot.java
@@ -1,9 +1,20 @@
 package com.example.appforros;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Robot {
     private int robot_id = 0;
     private long robot_ip;
     private String form_ip;
+    private Socket socket;
 
     public Robot() {
 
@@ -27,18 +38,75 @@ public class Robot {
         return form_ip;
     }
 
+    public boolean sendHello() {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket(form_ip, 12574);
+
+                    //向服务器发送数据
+                    PrintWriter send = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8")));
+                    send.println("hello");
+                    send.flush();
+
+                    //接受服务端数据
+                    BufferedReader recv = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String recvMsg = recv.readLine();
+                    if (recvMsg != null) {
+//                          mRecvText.setText(recvMsg);
+                        System.out.println(recvMsg);
+                    } else {
+//                          mRecvText.setText("Cannot receive data correctly.");
+                    }
+
+                    send.close();
+                    recv.close();
+                    socket.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        }).start();
+        return true;
+    }
+
     public void move(String direction) {
-        if (direction == "forward") {
+        final String sendMesg;
+        sendMesg = direction;
+        new Thread(new Runnable() {
 
-        } else if (direction  == "turnleft") {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket(form_ip, 12574);
 
-        } else if (direction == "turnright") {
+                    //向服务器发送数据
+                    PrintWriter send = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8")));
+                    send.println(sendMesg);
+                    send.flush();
 
-        } else if (direction == "backoff") {
+                    //接受服务端数据
+                    BufferedReader recv = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String recvMsg = recv.readLine();
+                    if (recvMsg != null) {
+//                          mRecvText.setText(recvMsg);
+                        System.out.println(recvMsg);
+                    } else {
+//                          mRecvText.setText("Cannot receive data correctly.");
+                    }
 
-        } else {
+                    send.close();
+                    recv.close();
+                    socket.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
 
-        }
+        }).start();
     }
 
 }
