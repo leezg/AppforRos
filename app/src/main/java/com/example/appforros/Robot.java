fc1531ba1f1095c14e31aa4a0bde16d2b0335413
leezg <2655421818@qq.com>
2020-05-22 10:32:49 +0800

    socket代码调整

--- app/src/main/java/com/example/appforros/Robot.java
+++ app/src/main/java/com/example/appforros/Robot.java
@@ -15,6 +15,7 @@ public class Robot {
     private long robot_ip;
     private String form_ip;
     private Socket socket;
+    private String recvMsg = null;
 
     public Robot() {
 
@@ -39,43 +40,20 @@ public class Robot {
     }
 
     public boolean sendHello() {
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    socket = new Socket(form_ip, 12574);
-
-                    //向服务器发送数据
-                    PrintWriter send = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8")));
-                    send.println("hello");
-                    send.flush();
-
-                    //接受服务端数据
-                    BufferedReader recv = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    String recvMsg = recv.readLine();
-                    if (recvMsg != null) {
-//                          mRecvText.setText(recvMsg);
-                        System.out.println(recvMsg);
-                    } else {
-//                          mRecvText.setText("Cannot receive data correctly.");
-                    }
-
-                    send.close();
-                    recv.close();
-                    socket.close();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-        }).start();
+        while (sendMessage("hello") == null) {
+            //等待收到应答确保发送完成
+        }
         return true;
     }
 
     public void move(String direction) {
-        final String sendMesg;
-        sendMesg = direction;
+        while (sendMessage(direction) == null) {
+            //等待收到应答确保发送完成
+        }
+    }
+
+    private String sendMessage (final String Msg){
+
         new Thread(new Runnable() {
 
             @Override
@@ -85,12 +63,12 @@ public class Robot {
 
                     //向服务器发送数据
                     PrintWriter send = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8")));
-                    send.println(sendMesg);
+                    send.println(Msg);
                     send.flush();
 
                     //接受服务端数据
                     BufferedReader recv = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    String recvMsg = recv.readLine();
+                    recvMsg = recv.readLine();
                     if (recvMsg != null) {
 //                          mRecvText.setText(recvMsg);
                         System.out.println(recvMsg);
@@ -107,6 +85,7 @@ public class Robot {
             }
 
         }).start();
+        return recvMsg;
     }
 
 }
